1cb6233591474521cd53829cd9a526d42ba27dfb
Rizki Wahyudi <115088806+RizkiWahyudi036@users.noreply.github.com>
2022-12-15 11:46:50 +0700

    Delete UAS directory

--- UAS/php/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "db_mahasiswa";
-$koneksi = mysqli_connect($host,$user,$pass) or die ("Koneksi database gagal");
-mysqli_select_db($koneksi,$name) or die ("Database Not Found");
-?>
